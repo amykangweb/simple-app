c0c3fe6842274e2480123a3079e96efa2815e042
amykangweb <selene6023@gmail.com>
2015-07-11 07:10:12 -0700

    Add shirts array and update css

--- shirts.php
+++ shirts.php
@@ -1,10 +1,26 @@
 <?php
 
-$products = array(
-  "Logo Shirt, Red",
-  "Mike the Frog Shirt, Black",
-  "Mike the Frog Shirt, Blue"
-);
+$products = array();
+$products[101] = array(
+  "name" => "Logo Shirt, Red",
+  "url" => "#",
+  "price" => 18
+  );
+$products[102] = array(
+  "name" => "Mike the Frog Shirt, Black",
+  "url" => "#",
+  "price" => 20
+  );
+$products[103] = array(
+  "name" => "Mike the Frog Shirt, Blue",
+  "url" => "#",
+  "price" => 25
+  );
+$products[104] = array(
+  "name" => "Logo Shirt, Green",
+  "url" => "#",
+  "price" => 15
+  );
 
 ?>
 <?php
@@ -14,10 +30,17 @@ include('header.php'); ?>
 
 <h1>Shirts For Sale</h1>
 
-<ul>
-  <?php foreach($products as $product) { ?>
-    <li><?php echo $product ?></li>
-  <?php } ?>
+<ul class="list">
+  <?php foreach($products as $product) {
+    echo"<li>";
+    echo'<a href="'. $product["url"] .'">';
+    echo $product["name"];
+    echo"</a>";
+    echo"<br>";
+    echo"<strong>Price:</strong>";
+    echo $product["price"];
+    echo"</li>";
+    } ?>
 </ul>
 
 <?php include('footer.php'); ?>
